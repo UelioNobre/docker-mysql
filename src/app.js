e55485425781f0b8fa2c44776450fd32d8ab05f5
uelionobre <uelionobre@hotmail.com>
2023-12-09 20:57:22 -0300

    Atualiza o app com as alterações

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const router = require('./routes')
+require('express-async-errors');
+
+const router = require('./routes');
+
+// Middleware de erro
+const errorMiddleware = require('./middlewares/error.middleware');
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
 
+// Implementa o middleware de erro
+app.use(errorMiddleware.errorHandler)
 
 module.exports = app
