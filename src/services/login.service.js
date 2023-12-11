14da207990833f8aa7aa4bb9d3427b42de436f5d
uelionobre <uelionobre@hotmail.com>
2023-12-10 23:46:20 -0300

    Remove validacao de email e senha no serviço de login

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -1,10 +1,8 @@
-const { validateEmailPassword } = require('../middlewares/validations/auth.validation')
 const tokenService = require('../services/token.service')
 const userService = require('../services/user.service')
 
 async function signin(email, password) {
   try {
-    await validateEmailPassword({ email, password });
     const user = await userService.findByEmailAndPassword(email, password)
     const token = tokenService.generate(user.message)
     return token
