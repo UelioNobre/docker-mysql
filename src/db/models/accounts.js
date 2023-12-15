0bc44bd4f3746da01edb3370c7646bd287693caf
uelionobre <uelionobre@hotmail.com>
2023-12-15 12:06:07 -0300

    Atualiza as definições dos campos da tabela Accounts e define o relacionamento para a tabela Users

--- src/db/models/accounts.js
+++ src/db/models/accounts.js
@@ -1,24 +1,33 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  class Accounts extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
+  class Accounts extends Model { }
+
   Accounts.init({
-    user_id: DataTypes.STRING,
-    status: DataTypes.STRING
+    user_id: DataTypes.INTEGER,
+    document: DataTypes.STRING,
+    document_type: {
+      type: DataTypes.STRING,
+      validate: {
+        isIn: {
+          args: [['cnpj', 'cpf']],
+          msg: "Only CPF or CPNJ documents"
+        },
+      }
+    },
+    status: DataTypes.BOOLEAN,
   }, {
     sequelize,
     modelName: 'Accounts',
+    underscored: false,
+    timestamps: false
   });
+
+  Accounts.associate = ({ User }) => {
+    Accounts.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
+  }
+
   return Accounts;
 };
